713e4d7e14a05224b20aaa636393e46bbfd406ab
장정우 <jungwoo@lotte.net>
2024-10-08 18:39:24 +0900

    first commit

---
+++ src/main/java/hello/itemservice/SpringDb2StudyApplication.java
@@ -0,0 +1,25 @@
+package hello.itemservice;
+
+import hello.itemservice.config.MemoryConfig;
+import hello.itemservice.repository.ItemRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+
+@Import(MemoryConfig.class)
+@SpringBootApplication(scanBasePackages = "hello.itemservice.web")
+public class SpringDb2StudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDb2StudyApplication.class, args);
+    }
+
+    @Bean
+    @Profile("local")
+    public TestDataInit testDataInit(ItemRepository itemRepository) {
+        return new TestDataInit(itemRepository);
+    }
+
+}
