705bf7eabba6ac9a5a02d3f172693cfc0cef9f6b
장정우 <jungwoo@lotte.net>
2024-10-21 18:35:50 +0900

    20241021 스프링 데이터 접근 기술 - MyBatis - 기타 기능

--- src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
+++ src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -15,6 +15,7 @@ public interface ItemMapper {
 
     void update(@Param("id") Long id, @Param("updateParam") ItemUpdateDto updateParam);
 
+//    @Select("select id, item_name, price, quantity from item where id=#{id}")
     Optional<Item> findById(Long id);
 
     List<Item> findAll(ItemSearchCond itemSearch);
